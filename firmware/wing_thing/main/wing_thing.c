2c0b082c1f71e64ad725e16684a1f2ffd9eb6a59
chris <chris1seto@gmail.com>
2024-02-12 15:48:49 -0800

    servo works

--- firmware/wing_thing/main/wing_thing.c
+++ firmware/wing_thing/main/wing_thing.c
@@ -8,14 +8,16 @@
 #include "mdns.h"
 #include "esp_http_server.h"
 #include "web.c"
-#include "esp_attr.h"
-#include "driver/mcpwm.h"
-#include "soc/mcpwm_periph.h"
+#include "driver/mcpwm_prelude.h"
 
 #define WIFI_SSID "Squawk7700"
 #define WIFI_PASS "yoloswaggins"
 
-#define SERVO_GPIO 18
+#define SERVO_PULSE_GPIO             18        // GPIO connects to the PWM signal line
+#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
+#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
+
+static const char *TAG = "thing";
 
 void SetServo(const uint32_t micros);
 
@@ -131,11 +133,11 @@ void wifi_init(void)
   esp_wifi_start();
 }
 
+mcpwm_cmpr_handle_t comparator = NULL;
+
 void SetServo(const uint32_t micros)
 {
-  mcpwm_set_frequency(MCPWM_UNIT_0, MCPWM_TIMER_0, 50); // 50Hz frequency
-  mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, micros); // Set duty cycle in microseconds
-  mcpwm_set_duty_type(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, MCPWM_DUTY_MODE_0); // Set duty cycle type
+  ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, micros));
 }
 
 void app_main(void)
@@ -153,10 +155,51 @@ void app_main(void)
 
   start_webserver();
 
-  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_GPIO);
+  ESP_LOGI(TAG, "Create timer and operator");
+  mcpwm_timer_handle_t timer = NULL;
+  mcpwm_timer_config_t timer_config = {
+    .group_id = 0,
+    .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+    .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
+    .period_ticks = SERVO_TIMEBASE_PERIOD,
+    .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+  };
+  ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
 
-  SetServo(1200);
+  mcpwm_oper_handle_t oper = NULL;
+  mcpwm_operator_config_t operator_config = {
+    .group_id = 0, // operator must be in the same group to the timer
+  };
+  ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
 
+  ESP_LOGI(TAG, "Connect timer and operator");
+  ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+
+  ESP_LOGI(TAG, "Create comparator and generator from the operator");
+
+  mcpwm_comparator_config_t comparator_config = {
+    .flags.update_cmp_on_tez = true,
+  };
+  ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+
+  mcpwm_gen_handle_t generator = NULL;
+  mcpwm_generator_config_t generator_config = {
+    .gen_gpio_num = SERVO_PULSE_GPIO,
+  };
+  ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+
+  ESP_LOGI(TAG, "Set generator action on timer and compare event");
+  // go high on counter empty
+  ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generator,
+  MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+  // go low on compare threshold
+  ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
+    MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+
+  ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+  ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+
+  SetServo(1000);
 
   while (true)
   {
